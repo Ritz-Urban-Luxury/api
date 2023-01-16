e01c0672efd13e1a38d566bb1f83912d4a7e8f45
Emmanuel Menyaga <sparklinsparky@gmail.com>
2023-01-16 14:03:13 +0100

    feat: init database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongooseDelete from 'mongoose-delete';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication';
@@ -11,6 +13,17 @@ import config from './shared/config';
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     LoggerModule,
     AuthenticationModule,
+    MongooseModule.forRoot(config().database.url, {
+      connectionFactory(connection) {
+        connection.plugin(mongooseDelete, {
+          overrideMethods: true,
+          deletedAt: true,
+        });
+        return connection;
+      },
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,6 +1,9 @@
 const config = () => ({
   env: process.env.NODE_ENV,
   port: Number.parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    url: process.env.DATABASE_URL,
+  },
 });
 
 export type Configuration = ReturnType<typeof config>;
