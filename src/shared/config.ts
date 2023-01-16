a7bab43519fd15bac68ba1cbbfd5e33c45c57aae
Emmanuel Menyaga <sparklinsparky@gmail.com>
2023-01-16 10:25:31 +0100

    feat: move access to env to config service

---
+++ src/shared/config.ts
@@ -0,0 +1,8 @@
+const config = () => ({
+  env: process.env.NODE_ENV,
+  port: Number.parseInt(process.env.PORT, 10) || 3000,
+});
+
+export type Configuration = ReturnType<typeof config>;
+
+export default config;
