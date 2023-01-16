a671451e0f73997a40138e93190c9ea6d3b287ca
Emmanuel Menyaga <sparklinsparky@gmail.com>
2023-01-16 11:38:55 +0100

    feat: setup payload parsing and helmet security

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { Logger } from './logger/logger.service';
+import cors from './shared/cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    cors,
+    bodyParser: true,
+  });
   const configService = app.get(ConfigService);
   const port = configService.get<string>('port');
   const logger = app.get(Logger);
 
   app.useLogger(logger);
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ limit: '100mb', extended: true }));
+  app.use(helmet());
 
   await app.listen(port);
   logger.log(`Running on port ${port}`);
