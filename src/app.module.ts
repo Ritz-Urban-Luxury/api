4d1de84c9fa5a9a9f0801f30d08553236963a56c
Emmanuel Menyaga <sparklinsparky@gmail.com>
2023-02-21 21:20:10 +0100

    debugging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
-import { /* CacheModule, */ Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import * as redisStore from 'cache-manager-redis-store';
-// import * as mongooseDelete from 'mongoose-delete';
-// import * as mongoosePaginate from 'mongoose-paginate-v2';
-// import { RedisClientOptions } from 'redis';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as redisStore from 'cache-manager-redis-store';
+import * as mongooseDelete from 'mongoose-delete';
+import * as mongoosePaginate from 'mongoose-paginate-v2';
+import { RedisClientOptions } from 'redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthenticationModule } from './authentication';
+// import { AuthenticationModule } from './authentication';
 // import { DatabaseModule } from './database/database.module';
 // import { FileModule } from './file/file.module';
 import { LoggerModule } from './logger/logger.module';
@@ -21,38 +21,38 @@ import config from './shared/config';
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     LoggerModule,
-    AuthenticationModule,
-    //     MongooseModule.forRoot(config().database.url, {
-    //       connectionFactory(connection) {
-    //         connection.plugin(mongoosePaginate);
-    //         connection.plugin(mongooseDelete, {
-    //           overrideMethods: true,
-    //           deletedAt: true,
-    //         });
-    //         return connection;
-    //       },
-    //       useNewUrlParser: true,
-    //       useUnifiedTopology: true,
-    //     }),
+    // AuthenticationModule,
+    MongooseModule.forRoot(config().database.url, {
+      connectionFactory(connection) {
+        connection.plugin(mongoosePaginate);
+        connection.plugin(mongooseDelete, {
+          overrideMethods: true,
+          deletedAt: true,
+        });
+        return connection;
+      },
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
     //     FileModule,
     //     PaymentModule,
-    //     CacheModule.registerAsync<RedisClientOptions>({
-    //       isGlobal: true,
-    //       useFactory() {
-    //         const { cache, redis } = config();
+    CacheModule.registerAsync<RedisClientOptions>({
+      isGlobal: true,
+      useFactory() {
+        const { cache, redis } = config();
 
-    //         if (redis.url) {
-    //           return {
-    //             ttl: cache.ttl,
-    //             url: redis.url,
-    //             password: redis.password,
-    //             store: redisStore as unknown as string,
-    //           };
-    //         }
+        if (redis.url) {
+          return {
+            ttl: cache.ttl,
+            url: redis.url,
+            password: redis.password,
+            store: redisStore as unknown as string,
+          };
+        }
 
-    //         return { ttl: cache.ttl };
-    //       },
-    //     }),
+        return { ttl: cache.ttl };
+      },
+    }),
     //     RidesModule,
     //     WebsocketModule,
     //     DatabaseModule,
