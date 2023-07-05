f7066e06ae498b5beb173d07f3d25b8634f9d8ab
Emmanuel Menyaga <sparklinsparky@gmail.com>
2023-07-05 11:31:55 +0100

    feat: endpoint to upload files

--- src/file/dto/file-upload.dto.ts
+++ src/file/dto/file-upload.dto.ts
@@ -1,11 +1,53 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  registerDecorator,
+} from 'class-validator';
+
+@ValidatorConstraint()
+class IsValidFileDataConstraint {
+  validate(value: string) {
+    if (!value || typeof value !== 'string') {
+      return false;
+    }
+
+    const [mime] = value.split('data:').pop().split(';');
+
+    return [
+      'image/png',
+      'image/jpeg',
+      // 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      // 'application/pdf',
+      // 'text/csv',
+    ].includes(mime);
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments) {
+    return `${validationArguments.property} must be a valid/supported file type`;
+  }
+}
+
+const IsValidFileData =
+  (validationOptions?: ValidationOptions) =>
+  (object: unknown, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsValidFileDataConstraint,
+    });
+  };
 
 export class FileUploadDTO {
   @IsString()
   @IsNotEmpty()
   filename: string;
 
-  @IsString()
+  @IsValidFileData()
   @IsNotEmpty()
   data: string;
 }
--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { FileService } from './file.service';
+import { FileController } from './file.controller';
 
 @Global()
 @Module({
   providers: [FileService],
   exports: [FileService],
+  controllers: [FileController],
 })
 export class FileModule {}
